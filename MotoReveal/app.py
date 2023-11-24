2a188f21dfce1c73262f508ece6231b739a18ed0
AndresQAgudelo <andres.quebrada@utp.edu.co>
2023-11-24 10:38:22 -0500

    Avance Registro, inicio y Menu, Seleccion de plan

--- MotoReveal/app.py
+++ MotoReveal/app.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import bcrypt
 from flask_bcrypt import Bcrypt
 from functools import wraps
+import string
 bcrypt = Bcrypt()
 
 app = Flask(__name__)
@@ -13,9 +14,13 @@ app.config['SECRET_KEY'] = 'clave_secreta'  # Cambia esto a una clave segura y s
 client = pymongo.MongoClient('mongodb://localhost:27017/users')
 db = client.user_login
 
-@app.route('/', )
+@app.route('/')
 def index():
-    return render_template('dashboard.html')
+    return render_template('index.html')
+
+@app.route('/buscar', methods=['POST', 'GET'])
+def buscar():
+    return render_template('menu.html')
 
 @app.route('/inicio', methods=['GET', 'POST'])
 def login():
@@ -38,37 +43,81 @@ def login():
 @app.route('/registrar',methods=['GET', 'POST'])
 def registrar():
     if request.method == 'POST':
-        usuario = request.form['name']
-        correo = request.form['email']
-        contraseña = request.form['password']
+        
+        date = {
+            'name'  : request.form.get('name'),
+            'email' : request.form.get('email'),
+            'phone' : request.form.get('phone'),
+            'date'  : request.form.get('date'),
+            'password'  : request.form.get('password'),
+   
+        }
+        
+        confirm = request.form.get('confirm')
+        
+        print(date)
+        # Password verification
+        validacion = validateCustomer(date, confirm) 
+        print(validacion)
+        if validacion:
+            return render_template('registro.html', validacion= validacion)
 
-        # Verificar si el usuario ya existe en la base de datos
-        if db.db.usuarios.find_one({'correo': correo}):
-            return 'Usuario ya registrado. <a href="/registro">Intentar nuevamente</a>'
+        # Get data from the card details section
+        plan = request.form.get('plan')
+        nameT = request.form.get('nameT')
+        number = request.form.get('number')
+        fecha = request.form.get('fecha')
+        ccv = request.form.get('ccv')
 
-        # Cifrar la contraseña antes de almacenarla en la base de datos
-        contraseña_cifrada = bcrypt.hashpw(contraseña.encode('utf-8'), bcrypt.gensalt())
+        # Save the data to your database or perform any necessary actions
+        # Here, we're just printing the data as an example
+        print(f"Name: {name}")
+        print(f"Email: {email}")
+        print(f"Date: {date}")
+        print(f"Plan: {plan}")
+        print(f"Name on Card: {nameT}")
+        print(f"Card Number: {number}")
+        print(f"Expiration Date: {fecha}")
+        print(f"CCV: {ccv}")
+
+        # Add your database logic or other actions here
+
+        return "Registration successful. Data has been processed."
 
 
-        print(usuario,correo,contraseña)
-        # Insertar el usuario en la base de datos
-        db.db.usuarios.insert_one({
-            'usuario': usuario,
-            'correo': correo,
-            'contraseña': contraseña_cifrada
-        })
-        session['logged_in'] = True
-        return redirect(url_for('home'))
-
-    return render_template('registro.html')
+    return render_template('registro.html', validacion = None)
 
 @app.route('/inicio')
 def inicio():
     return render_template('inicio.html')
 
-
-
-
+def validateCustomer(customer, confirm):
+    print(customer)
+    error_message = None
+    if not customer['name']:
+        error_message = "Please Enter your First Name !!"
+    elif len (customer['name']) < 3:
+        error_message = 'First Name must be 3 char long or more'
+    elif not customer['phone']:
+        error_message = 'Enter your Phone Number'
+    elif len (customer['phone']) < 10:
+        error_message = 'Phone Number must be 10 char Long'
+    elif len (customer['password']) < 7:
+        error_message = 'Password must be 7 char long'
+    elif not any([char.isdigit() for char in customer['password']]):
+        error_message = 'Password requires a digit'
+    elif not any([char.isupper() for char in customer['password']]):
+        error_message = 'Password requires a capital'
+    elif not any([True if char in string.punctuation else False for char in customer['password']]):
+        error_message = 'Password requires a special character'
+    elif len (customer['email']) < 5:
+        error_message = 'Email must be 5 char long'
+    elif customer.isExists ():
+        error_message = 'Email Address Already Registered..'
+    elif customer['password'] != confirm:
+        error_message  = 'The passwords do not match'
+    # saving
+    return error_message
 
 if __name__ == '__main__':
     app.run(debug=True)
